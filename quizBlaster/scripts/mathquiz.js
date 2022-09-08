0ed9e0a2eaec25674a30b6ea7a16e9a9f9cf6b53
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2022-09-08 10:35:38 +0200

    feat : external js

---
+++ quizBlaster/scripts/mathquiz.js
@@ -0,0 +1,8 @@
+const quiz={
+    name:"Math Quiz",
+    questions:[
+      {question:"Koliko ruka ima prstiju?",correctAnswer:"5"},
+      {question:"Koliko tjedan ima dana?",correctAnswer:"7"},
+      {question:"Napiši broj veći od 3 a manje od 5!",correctAnswer:"4"}
+    ]
+  }
