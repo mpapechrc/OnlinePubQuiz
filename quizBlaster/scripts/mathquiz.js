0d3fb31fd4428536cf16aa11d656fef1261583d6
mpapechrc <111442881+mpapechrc@users.noreply.github.com>
2022-09-11 23:25:07 +0200

    question 2.0 (#32)

--- quizBlaster/scripts/mathquiz.js
+++ quizBlaster/scripts/mathquiz.js
@@ -1,8 +1,8 @@
-const quiz={
+let quiz={
     name:"Math Quiz",
     questions:[
-      {question:"Koliko ruka ima prstiju?",correctAnswer:"5"},
-      {question:"Koliko tjedan ima dana?",correctAnswer:"7"},
-      {question:"Napiši broj veći od 3 a manje od 5!",correctAnswer:"4"}
+      {question:"Koliko ruka ima prstiju?",correctAnswer:"5",outcome: ""},
+      {question:"Koliko tjedan ima dana?",correctAnswer:"7",outcome: ""},
+      {question:"Napiši broj veći od 3 a manje od 5!",correctAnswer:"4",outcome: ""}
     ]
   }
